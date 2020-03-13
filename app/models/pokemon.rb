72463fca8eec9bde59ea4c5905010acffd0ab092
gdc <garret.dechavez@gmail.com>
2020-03-13 13:35:08 -0500

    added associations to models changed

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,4 @@
 class Pokemon < ApplicationRecord
   has_and_belongs_to_many :type
+  has_many :trainer_pokemon
 end
--- db/migrate/20200313180738_create_trainer_pokemons.rb
+++ db/migrate/20200313180738_create_trainer_pokemons.rb
@@ -1,7 +1,8 @@
 class CreateTrainerPokemons < ActiveRecord::Migration[6.0]
   def change
     create_table :trainer_pokemons do |t|
-
+      t.references :pokemon, null: true, foreign_key: true
+      t.references :trainer, null: true, foreign_key: true
       t.timestamps
     end
   end
