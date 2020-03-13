328adcea51af15b9c4a43dc6bf1346cb00ecb094
gdc <garret.dechavez@gmail.com>
2020-03-13 17:45:04 -0500

    matches all controller and view pages to work

--- app/controllers/evolutions_controller.rb
+++ app/controllers/evolutions_controller.rb
@@ -1,5 +1,5 @@
 class EvolutionsController < ApplicationController
-  before_action :set_evolution, only: [:show, :edit, :update, :destroy]
+  # before_action :set_evolution, only: [:show]
 
   # GET /evolutions
   # GET /evolutions.json
@@ -10,65 +10,6 @@ class EvolutionsController < ApplicationController
   # GET /evolutions/1
   # GET /evolutions/1.json
   def show
+    @evolution = Evolution.find(params[:id])
   end
-
-  # GET /evolutions/new
-  def new
-    @evolution = Evolution.new
-  end
-
-  # GET /evolutions/1/edit
-  def edit
-  end
-
-  # POST /evolutions
-  # POST /evolutions.json
-  def create
-    @evolution = Evolution.new(evolution_params)
-
-    respond_to do |format|
-      if @evolution.save
-        format.html { redirect_to @evolution, notice: 'Evolution was successfully created.' }
-        format.json { render :show, status: :created, location: @evolution }
-      else
-        format.html { render :new }
-        format.json { render json: @evolution.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /evolutions/1
-  # PATCH/PUT /evolutions/1.json
-  def update
-    respond_to do |format|
-      if @evolution.update(evolution_params)
-        format.html { redirect_to @evolution, notice: 'Evolution was successfully updated.' }
-        format.json { render :show, status: :ok, location: @evolution }
-      else
-        format.html { render :edit }
-        format.json { render json: @evolution.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /evolutions/1
-  # DELETE /evolutions/1.json
-  def destroy
-    @evolution.destroy
-    respond_to do |format|
-      format.html { redirect_to evolutions_url, notice: 'Evolution was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_evolution
-      @evolution = Evolution.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def evolution_params
-      params.require(:evolution).permit(:evolution_name, :evolved_from)
-    end
 end
--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -1,5 +1,5 @@
 class TrainersController < ApplicationController
-  before_action :set_trainer, only: [:show, :edit, :update, :destroy]
+  # before_action :set_trainer, only: [:show]
 
   # GET /trainers
   # GET /trainers.json
@@ -10,65 +10,6 @@ class TrainersController < ApplicationController
   # GET /trainers/1
   # GET /trainers/1.json
   def show
+    @trainer = Trainer.find(params[:id])
   end
-
-  # GET /trainers/new
-  def new
-    @trainer = Trainer.new
-  end
-
-  # GET /trainers/1/edit
-  def edit
-  end
-
-  # POST /trainers
-  # POST /trainers.json
-  def create
-    @trainer = Trainer.new(trainer_params)
-
-    respond_to do |format|
-      if @trainer.save
-        format.html { redirect_to @trainer, notice: 'Trainer was successfully created.' }
-        format.json { render :show, status: :created, location: @trainer }
-      else
-        format.html { render :new }
-        format.json { render json: @trainer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /trainers/1
-  # PATCH/PUT /trainers/1.json
-  def update
-    respond_to do |format|
-      if @trainer.update(trainer_params)
-        format.html { redirect_to @trainer, notice: 'Trainer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @trainer }
-      else
-        format.html { render :edit }
-        format.json { render json: @trainer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /trainers/1
-  # DELETE /trainers/1.json
-  def destroy
-    @trainer.destroy
-    respond_to do |format|
-      format.html { redirect_to trainers_url, notice: 'Trainer was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_trainer
-      @trainer = Trainer.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def trainer_params
-      params.require(:trainer).permit(:name, :description)
-    end
 end
